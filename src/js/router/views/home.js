a7032903c210358b866a779711fdaef00c281606
Martin Kruger <52622303+martink-rsa@users.noreply.github.com>
2024-09-28 00:16:33 +0200

    Added logout functionality

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,3 +1,10 @@
-import { authGuard } from "../../utilities/authGuard";
+import { clearAccessToken } from '../../api/auth/token';
+import { authGuard } from '../../utilities/authGuard';
 
 authGuard();
+
+const logoutButton = document.getElementById('logout-button');
+logoutButton.addEventListener('click', () => {
+  clearAccessToken();
+  authGuard();
+});
