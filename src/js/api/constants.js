a74053b9f8dca35e2c0c3a448d57d1df9cdd231c
Martin Kruger <52622303+martink-rsa@users.noreply.github.com>
2024-09-27 23:13:51 +0200

    Cleanup

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -18,10 +18,3 @@ export const API_SOCIAL = `${API_BASE}/social`;
 export const API_SOCIAL_POSTS = `${API_SOCIAL}/posts`;
 
 export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`;
-
-// const options = {
-//   headers: {
-//     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWFydGlua3J1Z2VyIiwiZW1haWwiOiJNYXJLcnU3NzY3OEBzdHVkLm5vcm9mZi5ubyIsImlhdCI6MTcyNzQ1MTk1Nn0.OrH9iPbcoyCASHOK8PF6NDmuBEbeZFwYmwNHTNjaIWY',
-//     'X-Noroff-API-Key': '6ea1ce54-b2f0-4d4a-96a3-dfb8999aa912'
-//   }
-// };
