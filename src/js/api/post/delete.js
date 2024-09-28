f9be3243177d92ae9e754435ee94dfa37ead9b7a
Martin Kruger <52622303+martink-rsa@users.noreply.github.com>
2024-09-28 15:27:02 +0200

    Added functionality to delete post

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,1 +1,17 @@
-export async function deletePost(id) {}
+import { doFetch } from '../../utilities/fetch';
+import { API_SOCIAL_POSTS } from '../constants';
+
+export async function deletePost(id) {
+  const url = `${API_SOCIAL_POSTS}/${id}`;
+  const method = 'DELETE';
+
+  try {
+    await doFetch({
+      url,
+      options: { method },
+      useAuth: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
